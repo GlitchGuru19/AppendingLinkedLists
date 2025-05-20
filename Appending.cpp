d806d3c36380b72cc23907fee7dafa2f4ada9254
GlitchGuru19 <peterkatebe12@gmail.com>
2025-05-20 21:27:43 +0200

    added a display function

--- Appending.cpp
+++ Appending.cpp
@@ -2,18 +2,21 @@
 
 using namespace std;
 
-struct Node
+class Node
 {
+public:
     string name;
+    string sin;
     Node *next;
 };
 
-void appendNode(Node *&head, string name)
+void appendNode(Node *&head, string name, string sin)
 {
     Node *newNode = new Node;
     Node *nodePtr = nullptr;
 
     newNode->name = name;
+    newNode->sin = sin;
     newNode->next = nullptr;
 
     if(head == nullptr)
@@ -32,12 +35,26 @@ void appendNode(Node *&head, string name)
 
 }
 
+void displayDetails(Node *&head)
+{
+    Node *current = nullptr;
+    current = head;
+    cout << "\n\nNames in the List" << endl << endl;
+    while(current)
+    {
+        cout << current->name << endl;
+        cout << current->sin << endl;
+        current = current->next;
+    }
+}
+
 int main()
 {
     Node *head = nullptr;
 
     int choice = 0;
     string name = "";
+    string sin = "";
     while(true)
     {
         cout << "1. Add Name" << endl;
@@ -50,19 +67,14 @@ int main()
             {
                 cout<< "Enter name = ";
                 cin >> name;
-                appendNode(head, name);
+                cout << "Enter SIN = ";
+                cin >> sin;
+                appendNode(head, name, sin);
                 break;
             }
         case 2:
             {
-                Node *current = nullptr;
-                current = head;
-                cout << "\n\nNames in the List" << endl << endl;
-                while(current)
-                {
-                    cout << current->name << endl;
-                    current = current->next;
-                }
+                displayDetails(head);
                 break;
             }
         }
